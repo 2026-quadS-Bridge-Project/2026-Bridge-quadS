cfe01e65a3a490af72ec0275727b0eb36b0f428e
soyoon0309 <soyoon0309@naver.com>
2026-04-02 19:20:42 +0900

    Feat: Parent,java Class fill
    mission domain setting을 위해 parent entity를 세팅했습니다.

--- src/main/java/me/sogom/bridge/domain/member/entity/Parent.java
+++ src/main/java/me/sogom/bridge/domain/member/entity/Parent.java
@@ -1,4 +1,31 @@
 package me.sogom.bridge.domain.member.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "parent")
 public class Parent {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "parent_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 12)
+    private String email;
+
+    @Column(nullable = false)
+    private String hash;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }

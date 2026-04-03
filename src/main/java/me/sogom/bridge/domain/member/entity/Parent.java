335e2ac2b7fa807081ea30e765a47f758545b3b2
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-04-03 15:44:58 +0900

    Feat : BaseEntity 생성

--- src/main/java/me/sogom/bridge/domain/member/entity/Parent.java
+++ src/main/java/me/sogom/bridge/domain/member/entity/Parent.java
@@ -1,17 +1,16 @@
 package me.sogom.bridge.domain.member.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import lombok.*;
+import me.sogom.bridge.domain.common.BaseEntity;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "parent")
-public class Parent {
+public class Parent extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "parent_id")
@@ -26,6 +25,4 @@ public class Parent {
     @Column(nullable = false)
     private String hash;
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
 }

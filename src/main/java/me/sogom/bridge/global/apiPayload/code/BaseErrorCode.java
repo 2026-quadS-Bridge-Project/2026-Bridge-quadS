4ce700cba4eb40acf20c2786380d296c6eed517c
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-04-05 21:40:48 +0900

    Feat : API 응답 통일

---
+++ src/main/java/me/sogom/bridge/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,10 @@
+package me.sogom.bridge.global.apiPayload.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
+}
---
+++ src/main/java/me/sogom/bridge/global/apiPayload/code/GeneralSuccessCode.java
@@ -0,0 +1,17 @@
+package me.sogom.bridge.global.apiPayload.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum GeneralSuccessCode implements BaseSuccessCode {
+
+    OK(HttpStatus.OK, "COMMON200", "요청이 성공적으로 처리되었습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}

19ba0d6aae8755266b3bcd2a945884e1f225d805
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-05-02 22:09:05 +0900

    Refactor: 불필요한 파일 삭제

--- src/main/java/me/sogom/bridge/domain/member/dto/RefreshRequest.java
+++
@@ -1,8 +0,0 @@
-package me.sogom.bridge.domain.member.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshRequest(
-        @NotBlank(message = "리프레시 토큰을 입력해 주세요.")
-        String refreshToken
-) {}

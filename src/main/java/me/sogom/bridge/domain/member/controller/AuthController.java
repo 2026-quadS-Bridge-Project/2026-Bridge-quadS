546ef583ccbf9ae93d68b8b2beb17e6d1598ae3d
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-05-02 18:49:03 +0900

    Refactor : DTO 통합 관리

--- src/main/java/me/sogom/bridge/domain/member/controller/AuthController.java
+++ src/main/java/me/sogom/bridge/domain/member/controller/AuthController.java
@@ -3,10 +3,8 @@ package me.sogom.bridge.domain.member.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.sogom.bridge.domain.member.code.MemberSuccessCode;
-import me.sogom.bridge.domain.member.dto.res.AuthResponse;
-import me.sogom.bridge.domain.member.dto.req.LoginRequest;
-import me.sogom.bridge.domain.member.dto.req.RefreshRequest;
-import me.sogom.bridge.domain.member.dto.req.SignUpRequest;
+import me.sogom.bridge.domain.member.dto.res.MemberResDTO;
+import me.sogom.bridge.domain.member.dto.req.MemberReqDTO;
 import me.sogom.bridge.domain.member.service.AuthService;
 import me.sogom.bridge.global.apiPayload.ApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,32 +20,32 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/parent/signup")
-    public ApiResponse<AuthResponse> signUpParent(@RequestBody @Valid SignUpRequest request) {
+    public ApiResponse<AuthResponse> signUpParent(@RequestBody @Valid MemberReqDTO.SignUpRequest request) {
         return ApiResponse.onSuccess(MemberSuccessCode.PARENT_SIGNUP_SUCCESS, authService.signUpParent(request));
     }
 
     @PostMapping("/children/signup")
-    public ApiResponse<AuthResponse> signUpChildren(@RequestBody @Valid SignUpRequest request) {
+    public ApiResponse<AuthResponse> signUpChildren(@RequestBody @Valid MemberReqDTO.SignUpRequest request) {
         return ApiResponse.onSuccess(MemberSuccessCode.CHILDREN_SIGNUP_SUCCESS, authService.signUpChildren(request));
     }
 
     @PostMapping("/parent/login")
-    public ApiResponse<AuthResponse> loginParent(@RequestBody @Valid LoginRequest request) {
+    public ApiResponse<AuthResponse> loginParent(@RequestBody @Valid MemberReqDTO.LoginRequest request) {
         return ApiResponse.onSuccess(MemberSuccessCode.PARENT_LOGIN_SUCCESS, authService.loginParent(request));
     }
 
     @PostMapping("/children/login")
-    public ApiResponse<AuthResponse> loginChildren(@RequestBody @Valid LoginRequest request) {
+    public ApiResponse<AuthResponse> loginChildren(@RequestBody @Valid MemberReqDTO.LoginRequest request) {
         return ApiResponse.onSuccess(MemberSuccessCode.CHILDREN_LOGIN_SUCCESS, authService.loginChildren(request));
     }
 
     @PostMapping("/token/refresh")
-    public ApiResponse<AuthResponse> refresh(@RequestBody @Valid RefreshRequest request) {
+    public ApiResponse<MemberResDTO.AuthResponse> refresh(@RequestBody @Valid MemberReqDTO.RefreshRequest request) {
         return ApiResponse.onSuccess(MemberSuccessCode.REFRESH_TOKEN_SUCCESS, authService.refresh(request));
     }
 
     @PostMapping("/logout")
-    public ApiResponse<Void> logout(@RequestBody @Valid RefreshRequest request) {
+    public ApiResponse<Void> logout(@RequestBody @Valid MemberReqDTO.RefreshRequest request) {
         authService.logout(request);
         return ApiResponse.onSuccess(MemberSuccessCode.LOGOUT_SUCCESS, null);
     }
--- src/main/java/me/sogom/bridge/domain/member/service/AuthService.java
+++ src/main/java/me/sogom/bridge/domain/member/service/AuthService.java
@@ -3,10 +3,8 @@ package me.sogom.bridge.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import me.sogom.bridge.domain.member.code.MemberErrorCode;
 import me.sogom.bridge.domain.member.MemberException;
-import me.sogom.bridge.domain.member.dto.res.AuthResponse;
-import me.sogom.bridge.domain.member.dto.req.LoginRequest;
-import me.sogom.bridge.domain.member.dto.req.RefreshRequest;
-import me.sogom.bridge.domain.member.dto.req.SignUpRequest;
+import me.sogom.bridge.domain.member.dto.req.MemberReqDTO;
+import me.sogom.bridge.domain.member.dto.res.MemberResDTO;
 import me.sogom.bridge.domain.member.entity.Children;
 import me.sogom.bridge.domain.member.entity.Parent;
 import me.sogom.bridge.domain.member.entity.RefreshToken;
@@ -34,7 +32,7 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     @Transactional
-    public AuthResponse signUpParent(SignUpRequest request) {
+    public MemberResDTO.AuthResponse signUpParent(MemberReqDTO.SignUpRequest request) {
         checkDuplicateEmail(request.email());
         Parent parent = Parent.builder()
                 .name(request.name())
@@ -42,11 +40,11 @@ public class AuthService {
                 .hash(passwordEncoder.encode(request.password()))
                 .build();
         parentRepository.save(parent);
-        return new AuthResponse(null, null);
+        return new MemberResDTO.AuthResponse(null, null);
     }
 
     @Transactional
-    public AuthResponse signUpChildren(SignUpRequest request) {
+    public MemberResDTO.AuthResponse signUpChildren(MemberReqDTO.SignUpRequest request) {
         checkDuplicateEmail(request.email());
 
         String code = ChildrenCodeGenerator.generateCode();
@@ -57,11 +55,11 @@ public class AuthService {
                 .code(code)
                 .build();
         childrenRepository.save(children);
-        return new AuthResponse(null, null);
+        return new MemberResDTO.AuthResponse(null, null);
     }
 
     @Transactional
-    public AuthResponse loginParent(LoginRequest request) {
+    public MemberResDTO.AuthResponse loginParent(MemberReqDTO.LoginRequest request) {
         Parent parent = parentRepository.findByEmail(request.email())
                 .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
         validatePassword(request.password(), parent.getHash());
@@ -69,7 +67,7 @@ public class AuthService {
     }
 
     @Transactional
-    public AuthResponse loginChildren(LoginRequest request) {
+    public MemberResDTO.AuthResponse loginChildren(MemberReqDTO.LoginRequest request) {
         Children children = childrenRepository.findByEmail(request.email())
                 .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
         validatePassword(request.password(), children.getHash());
@@ -77,7 +75,7 @@ public class AuthService {
     }
 
     @Transactional
-    public AuthResponse refresh(RefreshRequest request) {
+    public MemberResDTO.AuthResponse refresh(MemberReqDTO.RefreshRequest request) {
         String tokenValue = request.refreshToken();
 
         if (!jwtUtil.isValid(tokenValue)) {
@@ -97,11 +95,11 @@ public class AuthService {
     }
 
     @Transactional
-    public void logout(RefreshRequest request) {
+    public void logout(MemberReqDTO.RefreshRequest request) {
         refreshTokenRepository.deleteByToken(request.refreshToken());
     }
 
-    private AuthResponse issueTokens(AuthMember authMember) {
+    private MemberResDTO.AuthResponse issueTokens(AuthMember authMember) {
         String accessToken = jwtUtil.createAccessToken(authMember);
         String refreshTokenValue = jwtUtil.createRefreshToken(authMember);
 
@@ -115,12 +113,12 @@ public class AuthService {
                 .expiresAt(expiresAt)
                 .build());
 
-        return new AuthResponse(accessToken, refreshTokenValue);
+        return new MemberResDTO.AuthResponse(accessToken, refreshTokenValue);
     }
 
-    private AuthResponse issueAccessTokenOnly(AuthMember authMember) {
+    private MemberResDTO.AuthResponse issueAccessTokenOnly(AuthMember authMember) {
         String accessToken = jwtUtil.createAccessToken(authMember);
-        return new AuthResponse(accessToken, null);
+        return new MemberResDTO.AuthResponse(accessToken, null);
     }
 
     private AuthMember loadAuthMember(String email, MemberRole role) {
